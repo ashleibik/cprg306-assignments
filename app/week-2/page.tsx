f034b995ca26f1a21cd8deb8274d2e83142a7faf
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-22 14:25:00 -0700

    Clean up root page for Week 2

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,15 +1,10 @@
 import Link from "next/link";
-import StudentInfo from "./student.info";
 
-export default function Page() {
+export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <h2>Week 2 Assignment</h2>
-
-      <StudentInfo />
-
-      <Link href="/">Back to Home</Link>
+      <Link href="/week-2">Week 2 Assignment</Link>
     </main>
   );
 }

e1f0cb2f7f3dde7f01a9fea7248e9e2719f4e2a1
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-22 14:46:40 -0700

    Fix Week 2 link

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import StudentInfo from "./student.info";
 
-export default function Home() {
+export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+      <h1>Shopping List</h1>
+      <StudentInfo />
     </main>
   );
 }

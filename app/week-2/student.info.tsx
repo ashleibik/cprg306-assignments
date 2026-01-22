7660291afbcf4a9bc69ce46b0f478496363406e0
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-22 13:23:50 -0700

    Week 2 assignment complete

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Ahmed Shleibik</p>
+      <p>
+        <Link href="https://github.com/ashleibik/cprg306-assignments">
+          https://github.com/ashleibik/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}

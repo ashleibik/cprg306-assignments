1402dcf2be42924c8052e6b5316d5d3713f03c84
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-22 14:11:33 -0700

    Render StudentInfo on Week 2 page

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import StudentInfo from "./student.info";
 
-export default function Home() {
+export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+      <h2>Week 2 Assignment</h2>
+
+      <StudentInfo />
+
+      <Link href="/">Back to Home</Link>
     </main>
   );
 }

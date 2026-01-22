2e9c96b05488329d508aac71f0bf6444e80abeab
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-22 13:54:55 -0700

    Fix StudentInfo with my name and GitHub link

--- app/week-2/student.info.tsx
+++ app/week-2/student.info.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <div>
-      <p>Ahmed Shleibik</p>
+    <section>
+      <p>Name: Ahmed Shleibik</p>
       <p>
+        GitHub:{" "}
         <Link href="https://github.com/ashleibik/cprg306-assignments">
           https://github.com/ashleibik/cprg306-assignments
         </Link>
       </p>
-    </div>
+    </section>
   );
 }

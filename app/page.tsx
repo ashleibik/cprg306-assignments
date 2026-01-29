ae476889779122748087af52364c7be2071b7e07
Ahmed Shleibik <ashleibik@gmail.com>
2026-01-29 15:27:55 -0700

    Add Week 3 link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+    <main className="p-8">
+      <h1 className="text-2xl font-bold">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+
+      <ul className="list-disc ml-6 mt-4">
+        <li>
+          <Link href="/week-2">Week 2 Assignment</Link>
+        </li>
+        <li>
+          <Link href="/week-3">Week 3 Assignment</Link>
+        </li>
+      </ul>
     </main>
   );
 }
